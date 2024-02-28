db97517d00c481852c416a147e189a29dd348ca3
FoxySnake <matthox45@gmail.com>
2024-02-28 19:16:18 +0100

    fix deleted file

---
+++ Models/Result/RegisterResult.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using NewSky.API.Models.Dto;
+
+namespace NewSky.API.Models.Result
+{
+    public class RegisterResult
+    {
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
+        public bool IsSuccess => Errors.IsNullOrEmpty();
+    }
+}

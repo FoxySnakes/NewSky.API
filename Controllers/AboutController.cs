4f0953c2a13e8ad8a83adbbe86a15815ef59a502
FoxySnakes <matthox45@gmail.com>
2024-04-15 10:03:59 +0200

    fix

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewSky.API.Controllers
+{
+    [Route("[controller]")]
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
